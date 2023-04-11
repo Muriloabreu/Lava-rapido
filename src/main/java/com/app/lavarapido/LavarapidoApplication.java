e4dc851dc57f2b09ff9f4578319ee1e1020f39e7
muriloabreu <murilo.lilo@hotmail.com>
2023-04-10 22:05:28 -0300

    Inicialização Spring boot

---
+++ src/main/java/com/app/lavarapido/LavarapidoApplication.java
@@ -0,0 +1,13 @@
+package com.app.lavarapido;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LavarapidoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LavarapidoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/app/lavarapido/LavarapidoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.app.lavarapido;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LavarapidoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
